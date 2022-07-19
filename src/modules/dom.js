8bd11cda49fc4b4436590f3d0bd3b39d5ff1bf03
Cody Clements <subliminalverses2@gmail.com>
2022-07-18 22:16:50 -0500

    tweak design and finish nav functionality

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,13 +1,23 @@
 const domStuff = (() => {
 
 const hamburger = document.querySelector('.hamburger');
-const projectSection = document.querySelector('.projects')
+const projectSection = document.querySelector('.projects');
+const addTaskBtn = document.querySelector('.add-task');
+const projectExit = document.querySelector('.exit-btn')
 
 hamburger.addEventListener('click', () => {
-  projectSection.classList.toggle('active')
+  projectSection.classList.add('active')
 });
+
+addTaskBtn.addEventListener('click', () => {
+   addTaskBtn.classList.toggle('add-task-active');
+})
+
+projectExit.addEventListener('click', () => {
+  projectSection.classList.remove('active');
+})
 
 })();
 
 export default domStuff;
- 
+
